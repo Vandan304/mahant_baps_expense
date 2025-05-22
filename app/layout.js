310b33c57d4ac7d13ce6e1f4b296df0440c043d4
Vandans304 <vandansonicareers@gmail.com>
2025-05-22 18:04:18 +0530

    baps convex

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
+import {Inter} from "next/font/google"
+const inter = Inter({subsets:["latin"]});
 import "./globals.css";
+import Header from "@/components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "baps expense",
@@ -20,11 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/public/logos/logo-s.png" sizes="any" />
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
+      <Header />
         <main className="min-h-screen">
           {children}
         </main>
