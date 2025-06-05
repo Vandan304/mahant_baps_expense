6ea7eb8851f644546a8e94f77d11492802744a7d
Vandans304 <vandansonicareers@gmail.com>
2025-06-05 12:08:54 +0530

    baps

--- app/(main)/person/[id]/page.jsx
+++ app/(main)/person/[id]/page.jsx
@@ -2,9 +2,11 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { api } from "@/convex/_generated/api";
 import { useConvexQuery } from "@/hooks/use-convex-query";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, ArrowLeftRight, PlusCircle } from "lucide-react";
+import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { BarLoader } from "react-spinners";
@@ -53,8 +55,34 @@ const PersonPage = () => {
               <p className="text-muted-foreground">{otherUser?.email}</p>
             </div>
           </div>
+          <div className="flex gap-2">
+            <Button asChild variant="outline">
+              <Link href={`/settlements/user/${params.id}`}>
+                <ArrowLeftRight className="mr-2 h-4 w-4" />
+                Settle up
+              </Link>
+            </Button>
+            <Button asChild>
+              <Link href={`/expenses/new`}>
+                <PlusCircle className="mr-2 h-4 w-4" />
+                Add expense
+              </Link>
+            </Button>
+          </div>
         </div>
       </div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-xl">Balance</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex justify-between items-center">
+            <div>
+                
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
